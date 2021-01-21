de025e467a0917f06bf223c6641f79d1dff425c8
tm3k <60632198+tm3k@users.noreply.github.com>
2021-01-21 13:59:38 -0800

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,2 +1,9 @@
-APIkey = 'apikey'
-SecretKey = 'secretkey'
+#for binance
+APIkey = 'key'
+SecretKey = 'key'
+
+# for twitter
+consumer_key = 'key'
+consumer_secret = 'key'
+access_token = 'key'
+access_token_secret = 'key'
